8f7e9765a19f7f70f49cbcaceb0cd730c52359ff
ladams00 <ladams@sdf.lonestar.org>
2017-05-23 13:07:19 -0700

    Updated build script to produce full URLs for RPMs

--- Build/build-lxd.sh
+++ Build/build-lxd.sh
@@ -13,16 +13,17 @@ echo "Container name is $CONT"
 cat << EOF > prepincont.${RAND1}.sh
 #!/bin/bash
 echo "Preparing to build yum dependencies.."
-echo "Sleeping for 15 seconds to allow for container networking to settle (DHCP, DNS).."
-sleep 15
+echo "Sleeping for 10 seconds to allow for container networking to settle (DHCP, DNS).."
+sleep 10
 yum update
 yum install -y yum-utils
 mkdir -p /var/installroot
 rpm --root /var/installroot --initdb
+yumdownloader centos-release --destdir=/var/tmp --urls > /var/tmp/yum.deps.out
 yumdownloader centos-release --destdir=/var/tmp
 rpm --root /var/installroot -ivh --nodeps /var/tmp/centos-release*rpm
-yumdownloader yum --destdir=/var/tmp --resolve --installroot=/var/installroot
-ls -l /var/tmp
+yumdownloader yum --destdir=/var/tmp --resolve --installroot=/var/installroot --urls >> /var/tmp/yum.deps.out
+grep ^http /var/tmp/yum.deps.out | awk -F/ '{\$3="mirror.centos.org";print}' | tr ' ' '/' > /var/tmp/yum.deps.clean
 EOF
 
 echo "Pushing prepincont.${RAND1}.sh to container ${CONT}.."
@@ -32,6 +33,6 @@ rm prepincont.${RAND1}.sh
 echo "Running prepincont.${RAND1}.sh on container ${CONT}.."
 lxc exec ${CONT} -- /bin/bash prepincont.${RAND1}.sh
 echo "Retrieving list of yum dependencies"
-lxc exec ${CONT} -- ls /var/tmp | tee yum.bundle
+lxc exec ${CONT} -- cat /var/tmp/yum.deps.clean | tee yum.bundle
 echo "Removing ${CONT} forcefully.."
 lxc delete --force ${CONT}
