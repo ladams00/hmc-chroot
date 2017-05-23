c0afeef3ccdf97ff1c12b504d74a104b7350d8e9
ladams00 <ladams@sdf.lonestar.org>
2017-05-23 10:42:51 -0700

    Container build now produces Build/yum.bundle and removes container upon completion

--- Build/build-lxd.sh
+++ Build/build-lxd.sh
@@ -16,6 +16,13 @@ echo "Preparing to build yum dependencies.."
 echo "Sleeping for 15 seconds to allow for container networking to settle (DHCP, DNS).."
 sleep 15
 yum update
+yum install -y yum-utils
+mkdir -p /var/installroot
+rpm --root /var/installroot --initdb
+yumdownloader centos-release --destdir=/var/tmp
+rpm --root /var/installroot -ivh --nodeps /var/tmp/centos-release*rpm
+yumdownloader yum --destdir=/var/tmp --resolve --installroot=/var/installroot
+ls -l /var/tmp
 EOF
 
 echo "Pushing prepincont.${RAND1}.sh to container ${CONT}.."
@@ -24,5 +31,7 @@ echo "Cleaning up local copy of prepincont.${RAND1}.sh.."
 rm prepincont.${RAND1}.sh
 echo "Running prepincont.${RAND1}.sh on container ${CONT}.."
 lxc exec ${CONT} -- /bin/bash prepincont.${RAND1}.sh
+echo "Retrieving list of yum dependencies"
+lxc exec ${CONT} -- ls /var/tmp | tee yum.bundle
 echo "Removing ${CONT} forcefully.."
 lxc delete --force ${CONT}
