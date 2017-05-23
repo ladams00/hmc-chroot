b7f8b7e53aea7238853ef306ee137c2d5a4487ef
ladams00 <ladams@sdf.lonestar.org>
2017-05-23 09:47:03 -0700

    Starting skeleton of build process

--- Build/build-lxd.sh
+++ Build/build-lxd.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-# build.sh -- hmc-chroot/Build
+# build-lxd.sh -- hmc-chroot/Build
 #	Script to build yum dependencies under a CentOS6 LXD 
 #	container on Ubuntu
 
+RAND1=$RANDOM
 
-START=$(lxc launch images:centos/6/amd64) && CONT=$(echo $START | head -1 | awk '{print $2}'); echo "Container name is $CONT"
+echo "Launching container.."
+START=$(lxc launch images:centos/6/amd64) && \
+CONT=$(echo $START | head -1 | awk '{print $2}')
+echo "Container name is $CONT"
+
+cat << EOF > prepincont.${RAND1}.sh
+#!/bin/bash
+echo "Preparing to build yum dependencies.."
+echo "Sleeping for 15 seconds to allow for container networking to settle (DHCP, DNS).."
+sleep 15
+yum update
+EOF
+
+echo "Pushing prepincont.${RAND1}.sh to container ${CONT}.."
+lxc file push prepincont.${RAND1}.sh ${CONT}/root/
+echo "Cleaning up local copy of prepincont.${RAND1}.sh.."
+rm prepincont.${RAND1}.sh
+echo "Running prepincont.${RAND1}.sh on container ${CONT}.."
+lxc exec ${CONT} -- /bin/bash prepincont.${RAND1}.sh
+echo "Removing ${CONT} forcefully.."
+lxc delete --force ${CONT}
